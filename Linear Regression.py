8cd1e6f015f68e827658b4998cfa7016048de656
Kayzwer <lkyu753@gmail.com>
2021-09-30 23:37:54 +0800

    .

--- Linear Regression.py
+++ Linear Regression.py
@@ -1,13 +1,12 @@
 import numpy as np
 
-def gradient_descent(x, y, w, b, iteration):
-    learning_rate = 1e-3
-
+def gradient_descent(x, y, w, b, iteration, learning_rate):
+    n = len(x)
     for i in range(iteration):
         y_hat = w * x + b
-        loss = sum(y - y_hat) ** 2
-        dw = -2 * sum(x * (y - y_hat))
-        db = -2 * sum(y - y_hat)
+        loss = sum(y - y_hat) ** 2 / n
+        dw = -2 / n * sum(x * (y - y_hat))
+        db = -2 / n * sum(y - y_hat)
         w -= learning_rate * dw
         b -= learning_rate * db
         print(f"Epoch: {i + 1}, Loss: {loss}, Weight: {w}, Bias: {b} \n")
@@ -16,8 +15,7 @@ def gradient_descent(x, y, w, b, iteration):
 if __name__ == '__main__':
 
     x = np.array([1, 2, 3, 4, 5])
-    y = np.array([0.5, 1, 1.5, 2, 2.5])
+    y = np.array([2, 4, 6, 8, 10])
     w = 0
     b = 0
-
-    gradient_descent(x, y, w, b, 10000)
+    gradient_descent(x, y, w, b, 5000, 0.001)
